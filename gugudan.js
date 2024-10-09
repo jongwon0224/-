92a2b1c8ee66510bf56376682bd67a03cc15b9d3
Jong-Won <justine25@naver.com>
2024-10-09 15:59:55 +0900

    클래스를 활용한 문제풀이 3개

--- gugudan.js
+++ gugudan.js
@@ -1,78 +1,46 @@
-//for문을 사용해서 구구단...근데 인자를 html에서 직접받을수있나?? 찾아보자
-//ex) 숫자 몇을 몇단까지 하고 싶으신가요 ? 과 같은 사이트
-
-
-// 1. + 연산자 사용시 자동 숫자변화 ==>     var btn1 = +document.getElementById('btn1').value;
-// 2. Number()함수 사용 ==> 전체문자열이 숫자로 변환가능할때만 적용됨 ===>     var btn1 = Number(document.getElementById('btn1').value);
-//3. 유효성 검사 => typeof, isNaN(isNotaNumber)가 있음 isNaN(데이터)  ===> if(!isNaN(btn1) && !isNaN(btn2))
-//4. !는 부정을 의미 => !isNaN == number
-
-/* 수동계산  ==> Number함수 사용 */
-
-class button {
+//부모 버튼 클래스//
+class originButton {
     constructor(firstname, secondname, final) {
         this.firstname = Number(document.getElementById(firstname).value);
         this.secondname = Number(document.getElementById(secondname).value);
-        this.final = Number(document.getElementById(final).value);
+        this.final = document.getElementById(final);
     }
     test() {
         if (!isNaN(this.firstname) && !isNaN(this.secondname)) {
-            return this.final = this.firstname * this.secondname;
+            return this.final.value = this.firstname * this.secondname;
         } else {
-            return this.final = '';
+            return this.final.value = '';
         }
     }
 }
 
-function click() {
-    var manual = new button('btn1', 'btn2', 'result-btn');
+//수동버튼
+function handleClick() {
+    var manual = new originButton('btn1', 'btn2', 'result-btn');
     manual.test();
 }
 
-/*
-function startbtn() {
-    var btn1 = Number(document.getElementById('btn1').value);
-    var btn2 = Number(document.getElementById('btn2').value);
-    if (!isNaN(btn1) && !isNaN(btn2)) {
-        var result = btn1 * btn2;
-        document.getElementById('result-btn').value = result;
-    } else {
-        document.getElementById('result-btn').value = '';
-    }
+//자동버튼
+function autoClick() {
+    var automatic = new originButton('auto_btn1','auto_btn2','auto_result');
+    automatic.test();
 }
-
-/* 자동계산  ==> +기호 사용 */
-function gugu() {
-    var box1 = +document.getElementById('box1').value;
-    var box2 = +document.getElementById('box2').value;
-    if (!isNaN(box1) && !isNaN(box2)) {
-        var result = box1 * box2;
-        document.getElementById('boxresult').value = result;
-    } else {
-        document.getElementById('boxresult').value = '';
-    }
+/* 쉬운방법
+function magictxt () {
+    var writeP = document.getElementById('writeP').value;
+    var txtwrite = document.getElementById('txtwrite');
+    txtwrite.innerHTML = writeP;
 }
-
-//txt1박스 입력값을 p태그에 옮기기
-function autotyping() {
-    var clone = document.getElementById('txt1').value;
-    document.getElementById('val1').innerHTML = clone;
-}
-
-/* parseFloat란 함수 공부 필요
-<script>
-function startbtn() {
-    var btn1 = parseFloat(document.getElementById('btn1').value);
-    var btn2 = parseFloat(document.getElementById('btn2').value);
-
-    // 입력값이 숫자인지 확인
-    if (!isNaN(btn1) && !isNaN(btn2)) {
-        var result = btn1 * btn2;
-        document.getElementById('result-btn').value = result;
-    } else {
-        document.getElementById('result-btn').value = '유효하지 않은 입력';
-    }
-}
-</script>
 */
+// 클래스 사용
+class magic {
+    constructor(first, second) {
+        this.first = document.getElementById('writeP').value;
+        this.second = document.getElementById('txtwrite');
+    }
+}
 
+function magictxt () {
+    var magicbox = new magic('writeP','txtwrite');    
+    magicbox.second.innerHTML = magicbox.first;
+}
